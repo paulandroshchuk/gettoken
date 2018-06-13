a108f4f2615cbca09d660912f3a8e7d98edab76c
Paul A <paul.androshchuk@gmail.com>
2018-06-13 15:11:52 +0300

    Update templates.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,17 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">
+                    Navigation
+                </div>
+                <ul class="list-group list-group-flush token-navigation-menu">
+                    <a class="list-group-item" href="#">Dashboard</a>
+                    <a class="list-group-item" href="#">Team Management</a>
+                    <a class="list-group-item" href="#">Billing</a>
+                    <a class="list-group-item" href="#">Settings</a>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
                     You are logged in!
                 </div>
             </div>
