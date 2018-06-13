b40d3a3eaeb79f4c75abdb38d34abc97b80be621
Paul A <paul.androshchuk@gmail.com>
2018-06-13 15:55:47 +0300

    Update templates.

---
+++ resources/views/assets/sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <div class="card-header">
+        Navigation
+    </div>
+    <ul class="list-group list-group-flush token-navigation-menu">
+        <a class="list-group-item" href="#">Dashboard</a>
+        <a class="list-group-item" href="#">Team Management</a>
+        <a class="list-group-item" href="#">Billing</a>
+        <a class="list-group-item" href="#">Settings</a>
+    </ul>
+</div>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-9">
             <div class="card">
-                <div class="card-header">Home</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     You are logged in!
