dbfe152564e87850a99a18d5b81356b96689ea80
Paul A <paul.androshchuk@gmail.com>
2018-06-15 17:33:50 +0300

    Move up the API link.

--- resources/views/assets/sidebar.blade.php
+++ resources/views/assets/sidebar.blade.php
@@ -15,13 +15,13 @@
            href="{{ route('webhooks.index') }}">
             Webhooks
         </a>
-        <a class="list-group-item {{ isset($active) && $active === 'billing' ? 'active' : '' }}"
-           href="{{ route('billing.index') }}">
-            Billing
-        </a>
         <a class="list-group-item {{ isset($active) && $active === 'api' ? 'active' : '' }}"
            href="{{ route('api.index') }}">
             API
+        </a>
+        <a class="list-group-item {{ isset($active) && $active === 'billing' ? 'active' : '' }}"
+           href="{{ route('billing.index') }}">
+            Billing
         </a>
         <a class="list-group-item {{ isset($active) && $active === 'settings' ? 'active' : '' }}"
            href="{{ route('settings.index') }}">
