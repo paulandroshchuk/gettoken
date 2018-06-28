a4edcccb9b54c97e3c2a38bfe331cea60ddc38ac
Paul A <paul.androshchuk@gmail.com>
2018-06-28 23:05:56 +0300

    Get rid of the `user_id` field from the `tokens` table.

--- database/migrations/2018_06_18_195947_create_tokens_table.php
+++ database/migrations/2018_06_18_195947_create_tokens_table.php
@@ -15,18 +15,12 @@ class CreateTokensTable extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_id');
             $table->unsignedInteger('recipient_id');
             $table->unsignedInteger('gateway_id');
             $table->string('token');
             $table->enum('status', ['QUEUED', 'SENT', 'FAILED'])->default('QUEUED');
             $table->string('gateway_feedback')->nullable();
             $table->timestamps();
-
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('CASCADE');
 
             $table->foreign('gateway_id')
                 ->references('id')
