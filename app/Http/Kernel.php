61cce9de5fd70bc2a859a91cd70a2a690d76d9a2
Paul A <paul.androshchuk@gmail.com>
2018-07-01 01:12:29 +0300

    Refactor SetDefaultAcceptHeaderMiddleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'setDefaultAccept' => \App\Http\Middleware\SetDefaultAcceptHeaderMiddleware::class,
+        'setDefaultAccept' => \App\Http\Middleware\AcceptJsonMiddleware::class,
     ];
 }
